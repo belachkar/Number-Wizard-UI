790cc90b5074a0619e295a19ee5f1a272fc2362a
belachkar ali <34995109+belachkar@users.noreply.github.com>
2019-03-27 00:08:07 +0100

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+  [SerializeField] int max;
+  [SerializeField] int min;
+  [SerializeField] TextMeshProUGUI guessText;
+  int guess;
+
+  // Use this for initialization
+  void Start() {
+    StartGame();
+  }
+
+  void StartGame() {
+    NextGuess();
+  }
+
+  public void OnPressHigher() {
+    if (min < max) {
+      min = guess + 1;
+    } else {
+
+    }
+    NextGuess();
+  }
+
+  public void OnPressLower() {
+    if (min < max) {
+      max = guess - 1;
+    } else {
+      max = min;
+    }
+    NextGuess();
+  }
+
+  void NextGuess() {
+    guess = Random.Range(min, max + 1);
+    guessText.text = guess.ToString();
+  }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+  public void LoadNextScene() {
+    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+    SceneManager.LoadScene(currentSceneIndex + 1);
+  }
+  public void LoadStartScene() {    print("Load start scene");
+    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+    SceneManager.LoadScene("Start Menu");
+  }
+  public void QuitGame() {
+    Application.Quit();
+  }
+}
